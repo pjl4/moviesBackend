50761dcdc8b479fb3b24424a8fa4651f917a7664
ryansmane <ryanzimmer8@gmail.com>
2020-02-14 16:16:56 -0500

    adding cors, some routes

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -4,45 +4,57 @@ const Movie = require('../db/models/MovieModel');
 const Rating = require('../db/models/RatingsModel');
 
 router.get('/', (req, res) => {
-	Movie.find({}).then((movies) => {
-		res.json(movies);
-	});
+   Movie.find({}).then(movies => {
+      res.json(movies);
+   });
 });
 router.post('/', (req, res) => {
-	const newMovie = req.body;
-	Movie.create(newMovie)
-		.then((movie) => res.json(movie))
-		.catch(console.error);
+   const newMovie = req.body;
+   Movie.create(newMovie)
+      .then(movie => res.json(movie))
+      .catch(console.error);
 });
 router.get('/:id/rating', async (req, res) => {
-	const movieRatings = await Movie.findById(req.params.id).populate(
-		'ratings'
-	);
-	res.json(movieRatings.ratings);
+   const movieRatings = await Movie.findById(req.params.id).populate('ratings');
+   res.json(movieRatings.ratings);
 });
 router.post('/:id/rating', (req, res) => {
-	Rating.create(req.body).then((rating) => {
-		Movie.findById(req.params.id).then((movie) => {
-			movie.ratings.push(rating);
-			movie.save();
-			res.json(movie);
-		});
-	});
+   Rating.create(req.body).then(rating => {
+      Movie.findById(req.params.id).then(movie => {
+         movie.ratings.push(rating);
+         movie.save();
+         res.json(movie);
+      });
+   });
 });
+
+router.get('/:id/average', async (req, res) => {
+   const movieRatings = await Movie.findById(req.params.id).populate('ratings');
+   const reducer = (a, c) => a + c.rating;
+   const length = movieRatings.ratings.length;
+   const sum = movieRatings.ratings.reduce(reducer, 0);
+   const average = sum / length;
+   Movie.findByIdAndUpdate(
+      req.params.id,
+      { avgRating: average.toFixed(2) },
+      { new: true }
+   ).then(movie => res.json(movie));
+});
+
 router.get('/:id', (req, res) => {
-	Movie.findById(req.params.id)
-		.then((movie) => res.json(movie))
-		.catch(console.error);
+   Movie.findById(req.params.id)
+      .then(movie => res.json(movie))
+      .catch(console.error);
 });
 router.put('/:id', (req, res) => {
-	Movie.findByIdAndUpdate(req.params.id, req.body)
-		.then((movie) => res.json(movie))
-		.catch(console.error);
+   Movie.findByIdAndUpdate(req.params.id, req.body)
+      .then(movie => res.json(movie))
+      .catch(console.error);
 });
 router.delete('/:id', (req, res) => {
-	Movie.findOneAndDelete({ _id: req.params.id })
-		.then((movie) => res.json(movie))
-		.catch(console.error);
+   Movie.findOneAndDelete({ _id: req.params.id })
+      .then(movie => res.json(movie))
+      .catch(console.error);
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const PORT = 8080;
 const MovieController = require('./controllers/movieController');
 const UserController = require('./controllers/userController');
 const RatingController = require('./controllers/ratingsController');
-
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
