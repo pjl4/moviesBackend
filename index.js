5c5061ab9654d8debaa9222e2d4e164cb5f16263
pjl4 <pj.lutz17@gmail.com>
2020-02-19 11:49:36 -0500

    changes for deployment

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const PORT = 8080;
 const MovieController = require('./controllers/movieController');
 const UserController = require('./controllers/userController');
 const RatingController = require('./controllers/ratingsController');
-
-// app.use(function(req, res, next) {
-// 	res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
-// 	res.header(
-// 		'Access-Control-Allow-Headers',
-// 		'Origin, X-Requested-With, Content-Type, Accept'
-// 	);
-// 	next();
-// });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,6 +12,8 @@ app.use('/api/movies', MovieController);
 app.use('/api/user', UserController);
 app.use('/api/rating', RatingController);
 
-app.listen(PORT, () => {
-	console.log(`app running on ${PORT}`);
+app.set('port', process.env.PORT || 8080);
+
+app.listen(app.get('port'), () => {
+	console.log(`✅ PORT: ${app.get('port')} 🌟`);
 });
